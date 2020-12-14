cd89c4ce698b2c4034df9fa84c23e527de849835
Vladimirs Nordholm <v@vladde.net>
2020-12-14 13:19:59 +0100

    try new prompt

--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -1,4 +1,3 @@
 function fish_prompt --description 'Write out the prompt'
-    #printf '%s%s@%s%s%s%s%s> ' (set_color yellow) (whoami) (set_color purple) (prompt_hostname) (set_color $fish_color_cwd) (prompt_pwd) (set_color normal)
-    printf '%s%s %s%s %s%s%s $ ' (set_color green) (whoami) (set_color yellow) (prompt_hostname) (set_color blue) (prompt_pwd) (set_color normal)
+    printf '%s%s%s\n%s»%s ' (set_color blue) (prompt_pwd) (set_color normal) (set_color black) (set_color normal)
 end
