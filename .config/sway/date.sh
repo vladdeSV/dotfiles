eb1da6eab7dc4df03e0ca2498602dd7bfc42960c
Vladimirs Nordholm <v@vladde.net>
2026-03-16 18:02:16 +0100

    chore(sway): update weekday glyphs

--- .config/sway/date.sh
+++ .config/sway/date.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
 while true; do
-    case $(date +%u) in
-        1) week="▣▢▢▢▢▥▥" ;;
-        2) week="▢▣▢▢▢▥▥" ;;
-        3) week="▢▢▣▢▢▥▥" ;;
-        4) week="▢▢▢▣▢▥▥" ;;
-        5) week="▢▢▢▢▣▥▥" ;;
-        6) week="▢▢▢▢▢▣▥" ;;
-        7) week="▢▢▢▢▢▥▣" ;;
+    case "$(date +%u)" in
+        1) week="🞓🞎🞎🞎🞎🞔🞔" ;;
+        2) week="🞎🞓🞎🞎🞎🞔🞔" ;;
+        3) week="🞎🞎🞓🞎🞎🞔🞔" ;;
+        4) week="🞎🞎🞎🞓🞎🞔🞔" ;;
+        5) week="🞎🞎🞎🞎🞓🞔🞔" ;;
+        6) week="🞎🞎🞎🞎🞎🞓🞔" ;;
+        7) week="🞎🞎🞎🞎🞎🞔🞓" ;;
     esac
     echo "$week $(date +'%d %B %Y-%m-%d %R (:%S)')"
     sleep 1
