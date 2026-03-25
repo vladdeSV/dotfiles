9ce05c816fdcb3da579d9c67f141b8485ec8aac5
Vladimirs Nordholm <v@vladde.net>
2026-03-25 14:18:03 +0100

    wip: better

--- .config/sway/interactive-cal.sh
+++ .config/sway/interactive-cal.sh
@@ -13,21 +13,25 @@ done
 
 shift $((OPTIND - 1))
 
+day=$(date +%-d)
 month=$(date +%-m)
 year=$(date +%Y)
 
 original_month=$month
 original_year=$year
+offset=0
 
 
 #      |--------------------|
 legend='[jk] [r]         [q]'
 
+input="$(cat)"
+
 read_key() {
-  old=$(stty -g)
-  stty raw -echo
-  key=$(dd bs=1 count=1 2>/dev/null)
-  stty "$old"
+  old=$(stty -g < /dev/tty)
+  stty raw -echo < /dev/tty
+  key=$(dd bs=1 count=1 2>/dev/null < /dev/tty)
+  stty "$old" < /dev/tty
 }
 
 # initial draw, to handle relative cursor movement
@@ -37,7 +41,13 @@ first_print=1
 printf '\033[?25l' # hide cursor
 while true; do
   if [ $redraw -eq 1 ]; then
-    output=$("$program" --color=always "$month" "$year"; if [ "$hide_bar" -eq 0 ]; then printf '\033[7;37m%s\033[0m\r' "$legend"; fi)
+
+    cal_output=$(printf '%s' "$input" | "$program" --offset="$offset" "$day" "$month" "$year" 3>&1)
+    extra=$(printf '%s' "$cal_output" | tail -n +9)
+    cal_output=$(printf '%s' "$cal_output" | head -n 8)
+
+    output=$(echo "$cal_output"; if [ "$hide_bar" -eq 0 ]; then printf '\033[7;37m%s\033[0m\r' "$legend"; fi)
+
     move_up_lines=$(printf '%s' "$output" | wc -l)
     move_back_cols=$(echo "$output" | head -n 1 | wc -m)
     move_back_cols=$((move_back_cols - 2))
@@ -48,6 +58,12 @@ while true; do
       printf '\033[%dA\033[%sD' "$move_up_lines" "$move_back_cols"
     fi
     printf '%s' "$output"
+    if [ -n "$extra" ]; then
+      if [ "${#extra}" -gt 20 ]; then
+        extra="$(printf '%s' "$extra" | cut -c1-20)…"
+      fi
+      printf '%s' "$extra"
+    fi
   fi;
 
   read_key
@@ -56,24 +72,14 @@ while true; do
   case "$key" in
     n) ;;
     q) echo ; break ;;
-    k)
-      month=$((month - 1))
-      if [ "$month" -lt 1 ]; then
-        month=12
-        year=$((year - 1))
-      fi
-      ;;
-    j)
-      month=$((month + 1))
-      if [ "$month" -gt 12 ]; then
-        month=1
-        year=$((year + 1))
-      fi
-      ;;
-    r)
-      month=$original_month
-      year=$original_year
-      ;;
+
+    h) offset=$((offset - 1)) ;;
+    l) offset=$((offset + 1)) ;;
+    j) offset=$((offset + 7)) ;;
+    k) offset=$((offset - 7)) ;;
+    i) offset=$((offset - 28)) ;;
+    u) offset=$((offset + 28)) ;;
+    r) offset=0 ;;
     *) redraw=0 ;;
   esac
 done
